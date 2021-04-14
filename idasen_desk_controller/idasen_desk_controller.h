f2923b855c6e16fb837f168066530b45dc40ac9d
Julien Gobillot <jgobillot@deezer.com>
2021-04-14 19:17:39 +0200

    Add millimeter precision for height sensor

--- idasen_desk_controller/idasen_desk_controller.h
+++ idasen_desk_controller/idasen_desk_controller.h
@@ -16,7 +16,7 @@ class IdasenDeskControllerComponent : public Component, public cover::Cover, pub
   void use_bluetooth_callback(bool bluetooth_callback) { bluetooth_callback_ = bluetooth_callback; };
   void set_ble_device(BLEAdvertisedDevice *device);
 
-  void set_desk_height_sensor(sensor::Sensor *desk_height_sensor);
+  void set_desk_height_sensor(sensor::Sensor *desk_height_sensor) { desk_height_sensor_ = desk_height_sensor; };
   void set_desk_moving_binary_sensor(binary_sensor::BinarySensor *desk_moving_binary_sensor) {
     desk_moving_binary_sensor_ = desk_moving_binary_sensor;
   }
@@ -52,16 +52,15 @@ class IdasenDeskControllerComponent : public Component, public cover::Cover, pub
   BLERemoteCharacteristic *m_output_char_ = nullptr;
   BLERemoteCharacteristic *m_control_char_ = nullptr;
 
-  unsigned short height_target_ = 0;
+  float height_target_ = 0;
   cover::CoverOperation current_operation_{cover::COVER_OPERATION_IDLE};
 
   void scan_();
   void set_connection_(bool connected);
 
   void update_desk_();
-  unsigned short get_heigth_();
-  bool is_at_target_(unsigned short height) const;
-  void publish_cover_state_(unsigned short height);
+  float get_heigth_();
+  bool is_at_target_(float height) const;
   void publish_cover_state_(float position);
 
   void start_move_torwards_();
